a4bd8d0ebcd668b8f8abd20e5e97bcd8795398d3
Raffaele Spataro <raffaele2692@gmail.com>
2023-05-18 22:22:59 +0000

    logger

--- backend/common.py
+++ backend/common.py
@@ -2,8 +2,10 @@
 
 import json
 import os
+from datetime import datetime
 from loguru import logger as LOGGER
 
+LOGGER.add(f"logs/file_{datetime.now()}.log", rotation="10 MB")
 
 def set_env():
     with open("secrets.json", "r") as jsonfile:
